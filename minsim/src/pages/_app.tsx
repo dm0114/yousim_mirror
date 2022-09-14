c48e2352642a1798dbb6a02a25f372b16d234168
dm10802 <dm10802@gmail.com>
2022-09-14 18:08:48 +0900

    default settings

---
+++ minsim/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../../styles/globals.css'
+import type { AppProps } from 'next/app'
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://bf895e01895948d9b55d0bdc232962dd@o1409479.ingest.sentry.io/6745974",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
