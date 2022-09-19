d2571886d81e8e4b3537a66549e15c8e8cf8b7d8
dm10802 <dm10802@gmail.com>
2022-09-19 14:36:21 +0900

    navbar 이미지 및 router

--- minsim/src/components/NavBar.tsx
+++ minsim/src/components/NavBar.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '/public/images/yousimlogo.png'
+import { useRouter } from 'next/router';
+
 
 function NavBar() {
-
+    const router = useRouter()
+    const routerpush = () => {router.push("/")}
     return (
         <nav>
             <ul>
-                <li> Logo </li>
+                <li>
+                    <Image src={Logo} alt="" width={100} height={50} onClick={routerpush}></Image>
+                </li>
                 <li>
                     <Link href="/trend">트렌드</Link> 
                 </li>
