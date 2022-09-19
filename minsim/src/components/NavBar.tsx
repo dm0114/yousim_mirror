175e06acfc965cda71c7b0f8a2aec574e1a2c926
dm10802 <dm10802@gmail.com>
2022-09-19 15:52:59 +0900

    navbar css 수정

--- minsim/src/components/NavBar.tsx
+++ minsim/src/components/NavBar.tsx
@@ -3,22 +3,42 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Logo from '/public/images/yousimlogo.png'
 import { useRouter } from 'next/router';
+import styled from "@emotion/styled";
 
+const NavStyle = styled.nav`
+    /* position : fixed;
+    z-index : 1; */
+    width : 100%;
+`
+const NavUl = styled.ul`
+    display : flex;
+    list-style : none;
+    flex-direction : row;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 0;
+`
+
+const NavLi = styled.li`
+     display : inline-block;
+     text-transform : uppercase;
+` 
 
 function NavBar() {
     const router = useRouter()
     const routerpush = () => {router.push("/")}
     return (
-        <nav>
-            <ul>
-                <li>
+        <NavStyle>
+            <NavUl>
+                <NavLi></NavLi>
+                <NavLi>
                     <Image src={Logo} alt="" width={100} height={50} onClick={routerpush}></Image>
-                </li>
-                <li>
-                    <Link href="/trend">트렌드</Link> 
-                </li>
-            </ul>
-        </nav>
+                </NavLi>
+                <NavLi>
+                    <Link href="/trend">Trend</Link> 
+                </NavLi>
+            </NavUl>
+        </NavStyle>
     )
 }
 
