c3685193514eed1b59739b7ad7e9b60eaca1f77e
dm10802 <dm10802@gmail.com>
2022-09-20 09:59:56 +0900

    searchpage 수정중

---
+++ minsim/src/components/SearchList.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+
+const SearchListStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+`
---
+++ minsim/styles/SearchStyle.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const SloganContainer = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 210px;
+  margin-bottom: 64px;
+`
