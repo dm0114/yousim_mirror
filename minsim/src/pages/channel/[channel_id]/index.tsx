82098da0ccf0a700301f97720e1792f80035b165
dm10802 <dm10802@gmail.com>
2022-09-30 10:44:50 +0900

    fetch url 수정

--- minsim/src/pages/channel/[channel_id]/index.tsx
+++ minsim/src/pages/channel/[channel_id]/index.tsx
@@ -70,8 +70,8 @@ const ChannelDetailPage: NextPage = () => {
       return apiIniVideoList(query.channel_id);
     }
   );
-  // const {data, status} = useQuery(["videoData", videoId], ()=>{return apiChannelMinsim(videoId)})
-  console.log(chData);
+  const {data: channelMinsimData, status: minsimStatus} = useQuery(["minsim", query.channel_id], ()=>{return apiChannelMinsim(query.channel_id)})
+  console.log(channelMinsimData);
   
 
   return (
@@ -111,11 +111,11 @@ const ChannelDetailPage: NextPage = () => {
 
           <ChannelMinsimText
             title="채널 민심"
-            mainText="95%  떡상"
+            mainText={`${channelMinsimData} 떡상? 떡락?`}
           ></ChannelMinsimText>
           <ChannelMinsimText
-            title="가장 많이 언급된 키워드"
-            mainText="특화는 이게 맞아"
+            title="채널 키워드"
+            mainText={`${channelMinsimData} 떡상? 떡락?`}
           ></ChannelMinsimText>
         </section>
         <section>
