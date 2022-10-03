a863f055c75566bface845fee0d0d4740023c52f
dm10802 <dm10802@gmail.com>
2022-10-04 02:16:25 +0900

    key ch

--- src/main/java/com/ssafy/youtubeAnalysis/service/YoutubeServiceImpl.java
+++ src/main/java/com/ssafy/youtubeAnalysis/service/YoutubeServiceImpl.java
@@ -22,7 +22,7 @@ import java.util.Map;
 
 @Service("youtubeService")
 public class YoutubeServiceImpl implements YoutubeService {
-    public static final String KEY = "AIzaSyAU4mBYrU0O5IaKLKKBUuDxPxLLVotxTcg";
+    public static final String KEY = "AIzaSyBk4-MbPaB_PLYPKgx6i1lhckgGFTR0xwU";
 
     private static IWordAnalysisRepository wordAnalysisService;
 
