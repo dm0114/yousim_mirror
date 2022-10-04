88b7411e253897ae767a512a8d9df0a78ec8b834
dm10802 <dm10802@gmail.com>
2022-10-04 10:48:07 +0900

    Update YousimServiceImpl.java

--- src/main/java/com/ssafy/youtubeAnalysis/service/YousimServiceImpl.java
+++ src/main/java/com/ssafy/youtubeAnalysis/service/YousimServiceImpl.java
@@ -44,7 +44,7 @@ public class YousimServiceImpl implements YousimService {
     @Autowired
     TrendRepository trendRepository;
 
-    public static final String KEY = "AIzaSyCMqbJ4t_5SQ4CV-FCQp1IMMBiBi3nB5Z4";
+    public static final String KEY = "AIzaSyCaDz-hcLURYljbMnCw0rXOUB5jHPnxEek";
 
     static SparkConf sparkConf = new SparkConf().setAppName("simpleTest01")
             .setMaster("local").set("spark.driver.allowMultipleContexts", "true");
