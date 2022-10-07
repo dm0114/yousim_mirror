41aac164437ddd653ebcc20e119fc49b85db8d17
dm10802 <dm10802@gmail.com>
2022-10-07 09:48:14 +0900

    Update src/main/java/com/ssafy/youtubeAnalysis/service/YousimServiceImpl.java, src/main/java/com/ssafy/youtubeAnalysis/service/YoutubeServiceImpl.java

--- src/main/java/com/ssafy/youtubeAnalysis/service/YoutubeServiceImpl.java
+++ src/main/java/com/ssafy/youtubeAnalysis/service/YoutubeServiceImpl.java
@@ -24,7 +24,7 @@ import java.util.Map;
 public class YoutubeServiceImpl implements YoutubeService {
 
 
-    public static final String KEY = "AIzaSyAU4mBYrU0O5IaKLKKBUuDxPxLLVotxTcg";
+    public static final String KEY = "AIzaSyCaDz-hcLURYljbMnCw0rXOUB5jHPnxEek";
 
 
 
